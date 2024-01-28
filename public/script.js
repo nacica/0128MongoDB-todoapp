404de3614adc94a82638cea8fea07e9ddd9224a8
nacica <mikio.yokohama@gmail.com>
2024-01-29 07:28:58 +0900

    script.js修正

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const tasksDOM = document.querySelector(".tasks");
 const formDOM = document.querySelector(".task-form");
 const taskInputDOM = document.querySelector(".task-input");
